8a23fc23e297683877f41f6458ab43041507532a
Sviatoslav Simchuk <simchuksvyatoslav@gmail.com>
2026-07-02 13:26:49 +0300

    Added enums and entities

---
+++ src/TicketNotificationApi.Domain/Entities/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketNotificationApi.Domain.Enums;
+
+namespace TicketNotificationApi.Domain.Entities
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public Guid TicketId { get; set; }
+        public NotificationChannel Channel { get; set; }
+        public NotificationStatus Status { get; set; } = NotificationStatus.Pending;
+        public int Attempts { get; set; } = 0;
+        public string? LastError { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
---
+++ src/TicketNotificationApi.Domain/Entities/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketNotificationApi.Domain.Enums;
+
+namespace TicketNotificationApi.Domain.Entities
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public Priority Priority { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public List<Notification> Notifications { get; set; } = [];
+    }
+}
