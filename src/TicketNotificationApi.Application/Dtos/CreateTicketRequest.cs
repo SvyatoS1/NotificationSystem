8e7b723f3f9a975b2ef9fe18980e6955d66cf411
Sviatoslav Simchuk <simchuksvyatoslav@gmail.com>
2026-07-02 14:59:31 +0300

    Created DTOs

---
+++ src/TicketNotificationApi.Application/Dtos/CreateTicketRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketNotificationApi.Domain.Enums;
+
+namespace TicketNotificationApi.Application.Dtos
+{
+    public record CreateTicketRequest(
+        [Required]
+        [MinLength(5)]
+        string Title,
+        string? Description,
+        Priority Priority
+    );
+}
---
+++ src/TicketNotificationApi.Application/Dtos/MappingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketNotificationApi.Domain.Entities;
+
+namespace TicketNotificationApi.Application.Dtos
+{
+    public static class MappingExtensions
+    {
+        public static NotificationResponse ToResponse(this Domain.Entities.Notification notification)
+        {
+            return new NotificationResponse(
+                notification.Id,
+                notification.Channel,
+                notification.Status,
+                notification.Attempts,
+                notification.LastError,
+                notification.CreatedAt
+            );
+        }
+
+        public static TicketResponse ToResponse(this Ticket ticket)
+        {
+            return new TicketResponse(
+                ticket.Id,
+                ticket.Title,
+                ticket.Description,
+                ticket.Priority,
+                ticket.CreatedAt,
+                ticket.Notifications.Select(n => n.ToResponse()).ToList()
+            );
+        }
+    }
+}
