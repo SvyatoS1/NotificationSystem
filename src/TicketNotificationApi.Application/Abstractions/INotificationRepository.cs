9d9d910792b38e4b9d9cd5bcdf750644d095fc03
Sviatoslav Simchuk <simchuksvyatoslav@gmail.com>
2026-07-02 13:37:50 +0300

    Added abstraction layer for notifications

---
+++ src/TicketNotificationApi.Application/Abstractions/INotificationRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketNotificationApi.Domain.Entities;
+
+namespace TicketNotificationApi.Application.Abstractions
+{
+    public interface INotificationRepository
+    {
+        Task AddRangeAsync(IEnumerable<Notification> notifications);
+        Task<IEnumerable<Notification>> GetByTicketIdAsync(Guid ticketId);
+        Task UpdateAsync(Notification notification);
+    }
+}
---
+++ src/TicketNotificationApi.Application/Abstractions/ITicketRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketNotificationApi.Domain.Entities;
+
+namespace TicketNotificationApi.Application.Abstractions
+{
+    public interface ITicketRepository
+    {
+        Task AddAsync(Ticket ticket);
+        Task<Ticket?> GetByIdAsync(Guid id);
+    }
+}
