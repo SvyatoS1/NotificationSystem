f7133546207cda34002b7e2886f2bd013bfa50c9
Sviatoslav Simchuk <simchuksvyatoslav@gmail.com>
2026-07-02 16:10:04 +0300

    Remove unused references

--- src/TicketNotificationApi.Application/DependencyInjection.cs
+++ src/TicketNotificationApi.Application/DependencyInjection.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TicketNotificationApi.Application.Notifications;
-using TicketNotificationApi.Domain.Entities;
 
 namespace TicketNotificationApi.Application
 {
--- tests/TicketNotificationApi.Application.Tests/Fakes/FakeNotificationRepository.cs
+++ tests/TicketNotificationApi.Application.Tests/Fakes/FakeNotificationRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TicketNotificationApi.Application.Abstractions;
+﻿using TicketNotificationApi.Application.Abstractions;
 using TicketNotificationApi.Domain.Entities;
 
 namespace TicketNotificationApi.Application.Tests.Fakes
